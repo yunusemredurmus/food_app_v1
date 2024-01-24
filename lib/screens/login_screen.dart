6721cb8a80b0f5da57d8bb4163e1f157599cccdd
Yunus Emre Durmuş <ysemredurmus@gmail.com>
2024-01-24 14:43:48 +0300

    Add image and text field to login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,6 +12,7 @@ class LoginScreen extends StatelessWidget {
             image: AssetImage('assets/images/Pattern.png'),
             width: double.infinity,
             fit: BoxFit.cover,
+            height: 30,
           ),
           Padding(
             padding: EdgeInsets.all(8.0),
@@ -25,9 +26,38 @@ class LoginScreen extends StatelessWidget {
                 ),
                 Icon(Icons.person_outline, size: 30),
               ],
-              
             ),
-          )
+          ),
+          SizedBox(
+            width: 311,
+            height: 36,
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Search",
+                prefixIcon: Icon(Icons.search),
+                filled: true,
+                fillColor: Color.fromARGB(255, 233, 225, 225),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 233, 225, 225),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            child: Image(
+              image: AssetImage('assets/images/menu.png'),
+              width: 350,
+              height: 150,
+              fit: BoxFit.cover,
+            ),
+          ),
         ],
       ),
     );
